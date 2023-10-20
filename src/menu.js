8ae69153aa8b8ad7c94d48542a24da8f7534ef4a
Charlie <chrlie.w@proton.me>
2023-10-20 19:18:20 +0100

    Populated

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,59 @@
+function createElementWithClass(elementType, className) {
+    const element = document.createElement(elementType);
+    if (className) {
+      element.classList.add(className);
+    }
+    return element;
+  }
+  
+function createMenuItem(title, description, price) {
+    const menuCard = createElementWithClass('div', 'menu-card');
+    const dishTitle = createElementWithClass('h2', 'dish-title');
+    const dishDesc = createElementWithClass('p', 'dish-desc');
+    const dishPrice = createElementWithClass('p', 'dish-price');
 
-function build(){
-    // render content
-    const content = document.getElementById('content');
-    const main = document.querySelector('main');
+    dishTitle.textContent = title;
+    dishDesc.textContent = description;
+    dishPrice.textContent = price;
 
-    // create container
-    const container = document.createElement('div');
+    menuCard.appendChild(dishTitle);
+    menuCard.appendChild(dishDesc);
+    menuCard.appendChild(dishPrice);
 
-
-    content.appendChild(main);
-    main.appendChild(hero);
+    return menuCard;
 }
 
-export { build as buildMenu}
+function build() {
+    const main = document.querySelector('main');
+    const container = createElementWithClass('div', 'container');
+    const titleContainer = createElementWithClass('div', 'title-container');
+    const title = createElementWithClass('h1');
+    const subtitle = createElementWithClass('p', 'subtitle');
+
+    title.textContent = 'Golden Dragon';
+    subtitle.textContent = 'Menu';
+
+    titleContainer.appendChild(title);
+    titleContainer.appendChild(subtitle);
+
+    const menuContainer = createElementWithClass('div', 'menu-container');
+    const menuData = [
+        { title: 'Kung Pao Chicken', description: 'Spicy stir-fried chicken with peanuts', price: '$12' },
+        { title: 'Peking Duck', description: 'Crispy duck served with hoisin sauce and pancakes', price: '$18' },
+        { title: 'Mapo Tofu', description: 'Spicy tofu and minced meat in a flavorful sauce', price: '$10' },
+        { title: 'Dim Sum', description: 'Assortment of steamed and fried bite-sized dishes', price: '$15' },
+        { title: 'Szechuan Beef', description: 'Tender beef stir-fried with Szechuan peppercorns', price: '$14' },
+        { title: 'Hot and Sour Soup', description: 'Tangy and spicy soup with tofu and mushrooms', price: '$8' },
+    ];
+
+    menuData.forEach((item) => {
+        const menuCard = createMenuItem(item.title, item.description, item.price);
+        menuContainer.appendChild(menuCard);
+    });
+
+    main.appendChild(container);
+    container.appendChild(titleContainer);
+    container.appendChild(menuContainer);
+}
+
+export { build as buildMenu };
